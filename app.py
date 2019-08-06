ad55a248eab652c0080c143150dc472e99a71bf3
niv20-meet <nivhodtamir@Nivs-MacBook.local>
2019-08-06 17:39:12 +0300

    sd

--- app.py
+++ app.py
@@ -2,11 +2,16 @@
 from flask import Flask, render_template, url_for, redirect, request, session
 
 # Add functions you need from databases.py to the next line!
-from databases import add_student, get_all_students
+from database import *
+from flask import session as login_session
 
 # Starting the flask app
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'you-will-never-guess'
+# Login route
+# login func def
+# check credentials
+# set sesssion['user_id'] = user.id
 # App routing code here
 @app.route('/')
 def home():
@@ -30,7 +35,38 @@ def product_page():
 
 @app.route('/cart.html')
 def cart_page():
-	return render_template("cart.html")
+	items_dic = get_items(login_session['user_id'])
+	print(login_session['user_id'],"user id",items_dic)
+	items = []
+	for item in items_dic:
+		items.append(get_item(item['item_id']))
+	return render_template("cart.html", items = items)
+
+@app.route('/signup', methods=['POST',"GET"])
+def signup():
+	#check that username isn't already taken
+	if request.method == 'POST':
+		user = get_user(request.form['email'])
+		if user == None:
+			add_user(request.form['email'],request.form['psw'])
+			return home()
+	return render_template('singin.html')
+
+@app.route('/login', methods=['POST','GET'])
+def login():
+	if request.method == 'POST':
+		print("Got inside if" + request.method)
+		user = get_user(request.form['username'])
+		if user != None and user.verify_password(request.form["password"]):
+			login_session['name'] = user.username
+			login_session['user_id'] = user.id
+			login_session['logged_in'] = True
+	 		return home()
+	else:
+		return render_template('login.html')
+
+
+
 
 
 # Running the Flask app
--- database.py
+++ database.py
@@ -1,35 +1,34 @@
+from model import Base, User, Item, CartItem
 
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-Base = declarative_base()
+engine = create_engine('sqlite:///users.db?check_same_thread=False')
+Base.metadata.create_all(engine)
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
-class User(Base):
-	__tablename__ = 'users'
-	id = Column (Integer,primary_key=True)
-	username = Column(String)
-	passwors_hash = Column(String)
-	isadmin = Column(Boolean)
-	
-	def hash_password(self, password):
-	  self.password_hash = pwd_security.encrypt(password)
-    def verify_password(self, password):
-      return pwd_security.verify(password,self.password_hash)
+def add_user(name,secret_word):
 
-class Item(Base):
-	__tablename__ = 'items'
-	id = Column(Integer,primary_key=True)
-	pic = Column(String)
-	price = Column(Integer)
-	descrip = Column(String)
+	user = User(username=name)
+	user.hash_password(secret_word)
+	session.add(user)
+	session.commit()
 
 
-class cart_item(Base):
-		"""docstring for cart_item"""
-	id = Column(Integer,primary_key=True)
-	user_id = Column(Integer, ForeignKey('users.id'))
-	item_id = Column(Integer, ForeignKey('items.id'))
 
-	
+def get_user(username):
+	"""Find the first user in the DB, by their username."""
+	return session.query(User).filter_by(username=username).first()
+
+def delet_all_users():
+	session.query(User).delete()
+	session.commit()
+
+
+def get_items(user_id):
+	return session.query(CartItem).filter_by(user_id = user_id).all()
+
+def get_item(item_id):
+	return session.query(Item).filter_by(id = item_id).first()
+
